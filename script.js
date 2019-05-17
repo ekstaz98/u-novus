6b68d57a5baa5c4edd454259eb8f3ebc16654901
StanislavFateev <igroook1z2x3c@mail.ru>
2019-05-17 12:14:47 +0700

    final

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-//var mysql = require('mysql');
 function mysearch(){
-    var datas = document.getElementById("search").value;
-   $.ajax({
-     type: "GET",
-     url: "https://reqres.in/api/users/2"
+$.ajax({
+     url: "http://tmsk.aleks19.ru/api/tags",
     }).then(function(response){
+        var metki = JSON.parse(response);
+        $('#myt').html('');
+        var parent = document.getElementById('myt');
+        console.log(response);
+        for (var i = 0; i < metki.length; i++){
+            if(metki[i].status === false){
+              var status = "Не в архиве";
+              var ant = "";
+            }
+            else{
+              var status = "В архиве";
+              var ant = metki[i].ant;
+            }
+           var idm = metki[i].rfid;
+           var tr = document.createElement("tr");
+           var th = document.createElement("th");
+           var td1 = document.createElement("th");
+           var td2 = document.createElement("th");  
 
+           th.setAttribute('scope', 'row');
+           th.setAttribute('class', 'idm');
+           th.innerHTML = idm;
+           tr.appendChild(th);
 
-try {
-  var data = JSON.parse(response);
-  checkErrors(data);
-} catch (e) {
-  alert(e);
-}
+           td1.setAttribute('class', 'ant');
+           td1.innerHTML = ant;
+           tr.appendChild(td1);
 
-       /*var id = response.data.id[0]; 
-       var polka = response.data.mail[0];
-       var stel = response.data.avatar[0];*/
+           td2.setAttribute('class', 'status');
+           td2.innerHTML = status;
+           tr.appendChild(td2);
+
+           parent.appendChild(tr);
+       }
+       var time = setInterval(mysearch,3000);
     });
 }
-function show_(){
 
+//поиск по номеру антены ,но должно по названию документа -> не реализовано ,так как атрибута нет. (антен много, а документ 1 -> отображение только одной антены)
+function show_(){
+   $.ajax({
+     url: "http://tmsk.aleks19.ru/api/tags" 
+    }).then(function(response){
+        var poisk = JSON.parse(response);
+        var s = document.getElementById('result');
+        var str = document.getElementById('searchdoc').value;
+         for (var i = 0; i <= poisk.length; i++)
+         {
+             if(poisk[i].ant == str){
+                s.textContent = poisk[i].rfid+' '+ poisk[i].ant+' '+poisk[i].status;
+             }
+         }
+    });
 }
